8a3e2fcaa1836c066ea5f100ba95d4496a6b7d64
BrennanAndruss <brennana46274@stu.powayusd.com>
2022-08-19 14:03:39 -0700

    stage changes

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ public class Test
     public static void main(String[] args)
     {
         System.out.print("hi");
+        System.out.print(" hi");
+        System.out.print(" \n hello");
     }
 }
